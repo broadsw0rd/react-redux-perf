f4a2d70efea8855d7d5580e6ab732f99546a4007
Vladimir Bykov <broadsw.rd@yandex.ru>
2016-05-08 09:09:16 +0300

    Use redux-batched-subscribe; resolved #1

--- src/store.js
+++ src/store.js
@@ -1,7 +1,19 @@
 import { createStore } from 'redux'
+import { batchedSubscribe } from 'redux-batched-subscribe'
 
 import pairs from './reducers/pairs.js'
 
-var store = createStore(pairs)
+var requestAnimationFrame = window.requestAnimationFrame || window.webkitRequestAnimationFrame
+
+var lazyNotify = function () {}
+
+function digest () {
+  lazyNotify()
+  requestAnimationFrame(digest)
+}
+
+digest()
+
+var store = createStore(pairs, [], batchedSubscribe((nofity) => lazyNotify = nofity))
 
 export default store
